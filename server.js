c9060809ddafde10951408d657f9304c0477d3ba
Maxwell Cutri <mpcutri@gmail.com>
2018-04-03 18:54:02 -0600

    remove if else from mongoose connect

--- server.js
+++ server.js
@@ -43,12 +43,12 @@ app.use(routes);
 // added this from my file. (MAX) ===============
 mongoose.Promise = global.Promise
 let MONGO_URL
-const MONGO_LOCAL_URL = 'mongodb://localhost/ColoradoPours'
+// const MONGO_LOCAL_URL = 'mongodb://localhost/ColoradoPours'
 
-if (process.env.MONGODB_URI) {
-	mongoose.connect(process.env.MONGODB_URI)
-	MONGO_URL = process.env.MONGODB_URI
-} else {
+// if (process.env.MONGODB_URI) {
+// 	mongoose.connect(process.env.MONGODB_URI)
+// 	MONGO_URL = process.env.MONGODB_URI
+// } else {
 	// heroku_1zh96hjn - username
 	// 8ein2g5l10u4ctrrhlj7euo0kh - password
 	// ds127129.mlab.com - host
@@ -56,8 +56,8 @@ if (process.env.MONGODB_URI) {
 	// heroku_1zh96hjn - database
 	mongoose.connect("mongodb://heroku_1zh96hjn:8ein2g5l10u4ctrrhlj7euo0kh@ds127129.mlab.com:27129/heroku_1zh96hjn"); // local mongo url
 	var dbConnection = mongoose.connection
-	MONGO_URL = MONGO_LOCAL_URL
-}
+	// MONGO_URL = MONGO_LOCAL_URL
+// }
 // (MAX) ========================================
 app.use(
 	session({
