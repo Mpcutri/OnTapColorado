44991788dff03a45b2ef08bda3bb45a2003a8d39
IanBlackbeard <ianjnowicki@gmail.com>
2018-04-18 16:36:32 -0600

    user cards, user notification modal

--- client/src/pages/Detail/Detail.js
+++ client/src/pages/Detail/Detail.js
@@ -4,6 +4,16 @@ import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
+//Cards
+import { Card, CardImg, CardText, CardBody, CardDeck,
+  CardTitle, CardSubtitle, Button, Modal, ModalHeader, 
+  ModalBody, ModalFooter } from 'reactstrap';
+//DND
+import { DragDropContainer, DropTarget } from 'react-drag-drop-container';
+//Sortable
+import {render} from 'react-dom';
+import {SortableContainer, SortableElement} from 'react-sortable-hoc';
+import "./Detail.css";
 
 const style = {
   breweryList: {
@@ -21,12 +31,52 @@ const style = {
     float: 'none'
   }
 }
+//Modal
+// class ModalExample extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       modal: false
+//     };
+
+//     this.toggle = this.toggle.bind(this);
+//   }
+
+//   toggle() {
+//     this.setState({
+//       modal: !this.state.modal
+//     });
+//   }
+// }
 
 class Detail extends Component {
-  state = {
-    brewery: "",
-    beers: []
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      brewery: "",
+      beers: [],
+      backdrop: true,
+    };
+
+    this.toggle = this.toggle.bind(this);
+    this.changeBackdrop = this.changeBackdrop.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  changeBackdrop(e) {
+    let value = e.target.value;
+    if (value !== 'static') {
+      value = JSON.parse(value);
+    }
+    this.setState({ backdrop: value });
+  }
+
   // When this component mounts, grab the brewery with the _id of this.props.match.params.id
   // e.g. localhost:3000/breweries/599dcb67f0f16317844583fc
   componentDidMount() {
@@ -38,12 +88,13 @@ class Detail extends Component {
   handleClick = (e) => {
     e.preventDefault();
     window.location = this.state.brewery.website;
-    console.log('The link was clicked.');
+    console.log('Link to brewery URL has been clicked.');
   }
 
   render() {
     return (
       <Container fluid>
+        {/* Jumbotron */}
         <Row>
           <Col size="md-12">
             <Jumbotron>
@@ -57,62 +108,54 @@ class Detail extends Component {
                 {this.state.brewery.phone_number}
               </p>
               <p>
-                <a 
-                  onClick={this.handleClick}>
-                  {this.state.brewery.website}
-                </a>
+                <Button onClick={this.handleClick}>
+                  Brewery Website
+                </Button>
               </p>
             </Jumbotron>
           </Col>
         </Row>
-
+        {/* Beers on tap list*/}
         <Row>
           <Col size="md-10 md-offset-1">
             <article>
-              <h1>Brews currently on tap</h1>
+              <h1>On Tap Right Now!!!</h1>
             </article>
             {this.state.beers.length ? (
-              <div className="brewery-list" style={style.breweryList}>
-                <List>
-                  {this.state.beers.map(beer => (
-                    <ListItem key={beer._id}>
-                      <Row>
-                        <Col size="md-5 md-offset-1">
-                          <strong>
-                          <h2>
-                            {beer.name}
-                          </h2>
-                          </strong>
-                          <p>
-                            Type: {beer.type}
-                          </p>
-                          <p>
-                            ABV: {beer.abv}
-                          </p>
-                          <p>
-                            IBU: {beer.ibu}
-                          </p>
-                        </Col>
-                        <Col size="md-5 md-offset-1">
-                          <strong>
-                          <p>
-                            Description
-                          </p>
-                          </strong>
-                          <p>
-                            {beer.description}
-                          </p>
-                        </Col>
-                      </Row>
-                      
-                    </ListItem>
+              <CardDeck>
+                  {this.state.beers.map(beer => (                     
+                    <Col size="sm-3">
+                        <DragDropContainer>
+                          <Card key={beer._id}>
+                            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                            <CardBody>
+                              <CardTitle>{beer.name}</CardTitle>
+                              <CardSubtitle marginBottom="1000px">{beer.type}</CardSubtitle>
+                              <CardText>&#9632; ABV:{beer.abv} &#9632; IBU:{beer.ibu}</CardText>
+                              <CardText>{beer.description}</CardText>
+                              {/* Notification button opens modal */}
+                              <Button color="primary" onClick={this.toggle}>Notifications</Button>
+                              {/* Actual modal */}
+                              <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} backdrop={this.state.backdrop}>
+                                <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                                <ModalBody>
+                              {/* Form goes here*/}
+                                form goes here for user to enter in email or phone # to be notified of state change - inventory > on tap
+                                </ModalBody>
+                                <ModalFooter>
+                                  <Button color="primary" onClick={this.toggle}>Submit</Button>
+                                </ModalFooter>
+                              </Modal>
+                            </CardBody>
+                          </Card>
+                        </DragDropContainer>
+                    </Col>
                   ))}
-                </List>
-              </div>
+
+              </CardDeck>
             ) : (
               <h3>No Results to Display</h3>
             )}
-
           </Col>
         </Row>
       </Container>
